64dcf223ac8e18e0255e10004b8056b8857720a9
jmsharp00 <jessica.m.sharp00@gmail.com>
2025-01-22 19:35:42 +0900

    Delete src directory

--- src/main/java/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class Constants {
-        public static final String DRIVER_PATH = "path/to/chromedriver";
-        public static final String URL = "http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login";
-        public static final String USERNAME = "Admin";
-        public static final String PASSWORD = "Hum@nhrm123";
-}
